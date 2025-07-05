0a8a5a2730b1ccb3fb6a9fc6be9b92dc5e33f932
nota0ua <demiddubrovskylite@gmail.com>
2025-07-05 21:06:51 +0300

    New file structure

--- src/__main__.py
+++ src/__main__.py
@@ -1,4 +1,16 @@
-if __name__ == "__main__":
-    from src.api import create_models
+import asyncio
+from src.api.models_generator import ModelGenerator, LocalFileWriter
+from src.api import AsyncHTTPClient
 
-    create_models()
+
+async def main():
+    generator = ModelGenerator(
+        openapi_url="https://api.artifactsmmo.com/openapi.json",
+        models_path="./src/api/models",
+        http_client=AsyncHTTPClient(),
+        file_writer=LocalFileWriter()
+    )
+    await generator.generate_models()
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ src/api/models_generator/file_writer.py
@@ -0,0 +1,14 @@
+from typing import Protocol
+from pathlib import Path
+
+
+class FileWriterProtocol(Protocol):
+    def write(self, file_path: str, content: str) -> None:
+        pass
+
+
+class LocalFileWriter:
+    def write(self, file_path: str, content: str) -> None:
+        Path(file_path).parent.mkdir(parents=True, exist_ok=True)
+        with open(file_path, "w") as f:
+            f.write(content)
