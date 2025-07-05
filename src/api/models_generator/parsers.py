4cc643324cac99c5d919f1fb230373f0723860f0
nota0ua <demiddubrovskylite@gmail.com>
2025-07-05 21:35:47 +0300

    Bug fix

--- src/api/models_generator/parsers.py
+++ src/api/models_generator/parsers.py
@@ -95,7 +95,7 @@ class {enum_name}(StrEnum):
 
 class DataPageSchemaParser(SchemaParser):
     def __init__(self):
-        self.model_template = """from src.api.utils import DataPage
+        self.model_template = """from src.api.datapage import DataPage
 from .{ref_type_snake} import {ref_type}
 
 class {datapage_name}(DataPage):
