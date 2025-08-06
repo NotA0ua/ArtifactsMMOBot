fa1386da053e0a3acd96277f50e0d8f14e4ea770
NotA0ua <demiddubrovskylite@gmail.com>
2025-08-06 15:31:14 +0300

    0.1.28 Small __init__ fix

--- src/api/generators/__init__.py
+++ src/api/generators/__init__.py
@@ -2,7 +2,6 @@ import logging
 from re import sub
 from typing import Any
 
-# TODO: Check what is imported from it
 from .file import FileWriterProtocol, LocalFileWriter
 from src.api.client import HTTPClientProtocol
 from .models_generator import (
@@ -136,3 +135,6 @@ class {endpoint_name}:
     def _camel_to_snake(name: str) -> str:
         name = sub(r"(.)([A-Z][a-z]+)", r"\1_\2", name)
         return sub(r"([a-z0-9])([A-Z])", r"\1_\2", name).lower()
+
+
+__all__ = ["LocalFileWriter"]
